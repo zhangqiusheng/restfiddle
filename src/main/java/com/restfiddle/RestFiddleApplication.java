1b671153c7158707dbef7cd175fd4301214b7bb2
zhangqiusheng <zhangqiusheng@gridsum.com>
2017-01-24 17:07:15 +0800

    fix 启动异常
    delete 多余的private method

--- src/main/java/com/restfiddle/RestFiddleApplication.java
+++ src/main/java/com/restfiddle/RestFiddleApplication.java
@@ -25,8 +25,6 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan
 public class RestFiddleApplication {
 
-    private RestFiddleApplication() {}
-
     public static void main(String[] args) throws Exception {
 	SpringApplication.run(RestFiddleApplication.class, args);
     }
